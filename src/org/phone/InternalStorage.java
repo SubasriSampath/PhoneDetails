65f1c2ff3a8fbf278f41e22a2e4d14e9d8380e72
SubasriSampath <subasrivasanthan@gmail.com>
2022-09-22 19:42:16 +0530

    new code update [skip ci]

--- src/org/phone/InternalStorage.java
+++ src/org/phone/InternalStorage.java
@@ -5,10 +5,10 @@ private void processorName() {
 	System.out.println("Processor Name is SNAPDRAGON 888 ");
 }
 private void ramSize() {
-	System.out.println("Ram Size is 8GB");
+	System.out.println("Ram Size is 16GB");
 }
 private void phoneName() {
-	System.out.println("Phone Name is Redmi");
+	System.out.println("Phone Name is Vivo");
 }
 public static void main(String[] args) {
 	InternalStorage I = new InternalStorage();
