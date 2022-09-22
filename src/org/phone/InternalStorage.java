2aa0d064d3f4ecdf30527ec84f90af988369efb9
SubasriSampath <subasrivasanthan@gmail.com>
2022-09-22 19:06:33 +0530

    Rom

--- src/org/phone/InternalStorage.java
+++ src/org/phone/InternalStorage.java
@@ -6,6 +6,7 @@ private void processorName() {
 }
 private void ramSize() {
 	System.out.println("Ram Size is 8GB");
+	System.out.println("Rom size is 1TB");
 }
 public static void main(String[] args) {
 	InternalStorage I = new InternalStorage();
